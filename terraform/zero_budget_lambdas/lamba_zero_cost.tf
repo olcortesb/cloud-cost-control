f58d1090391e7e2a696b96567185cdb6dc952523
olcortesb <olcortesb@gmail.com>
2025-03-17 20:23:51 +0100

    feat: add lambda sample budget

--- terraform/zero_budget_lambdas/lamba_zero_cost.tf
+++ terraform/zero_budget_lambdas/lamba_zero_cost.tf
@@ -1,26 +1,21 @@
-resource "aws_budgets_budget" "lambda_zero_cost" {
+resource "aws_budgets_budget" "lambda_zero_cost_test" {
   name              = "${var.environment}-lambda-zero-cost-budget"
   budget_type       = "COST"
-  limit_amount      = "0"
+  limit_amount      = "0.01"
   limit_unit        = "USD"
   time_period_start = "2024-01-01_00:00"
   time_unit         = "MONTHLY"
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 0
+    threshold                  = 50
     threshold_type             = "ABSOLUTE_VALUE"
     notification_type          = "ACTUAL"
-    subscriber_email_addresses = [var.alert_email]
+    subscriber_email_addresses = var.mail_subscriber
   }
 
   cost_filter {
     name   = "Service"
     values = ["AWS Lambda"]
   }
-}
-
-variable "alert_email" {
-  description = "Email address to receive budget alerts"
-  type        = string
 }
---
+++ terraform/zero_budget_lambdas/variables.tf
@@ -0,0 +1,65 @@
+variable "platform_name" {
+  type    = string
+  default = "cost-control"
+  validation {
+    condition     = can(regex("^[a-z0-9\\-]+$", var.platform_name))
+    error_message = "The value must be a valid platform name."
+  }
+}
+
+variable "environment" {
+  type    = string
+  default = "test"
+  validation {
+    condition     = can(regex("^[a-z0-9\\-]+$", var.environment))
+    error_message = "The value must be a valid environment name."
+  }
+}
+
+variable "limit_amount" {
+  type        = string
+  default     = "1"
+  description = "Value in dollars of the limit amount per month"
+  validation {
+    condition     = can(regex("^[0-9]+\\.?[0-9]*$", var.limit_amount))
+    error_message = "The value must be a number."
+  }
+}
+
+variable "mail_subscriber" {
+  type    = list(string)
+  default = ["yourmail@some.com"]
+  validation {
+    condition = alltrue([
+      for i in var.mail_subscriber : can(regex("^[a-z0-9\\._\\-]+\\@([a-z0-9\\-]+\\.)+[a-z]{2,4}$", i))
+    ])
+    error_message = "The value must be a valid email address."
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+  validation {
+    condition     = can(regex("^[a-z0-9\\-]+$", var.aws_region))
+    error_message = "The value must be a valid AWS region name."
+  }
+}
+
+variable "s3-bucket" {
+  type    = string
+  default = "bucket-name"
+  validation {
+    condition     = can(regex("^[a-z0-9][a-z0-9\\-]*[a-z0-9]$", var.s3-bucket))
+    error_message = "The value must be a valid bucket name."
+  }
+}
+
+variable "threshold" {
+  type    = number
+  default = 50
+  validation {
+    condition     = var.threshold <= 100 && var.threshold >= 0
+    error_message = "The threshold must be a number between 0 and 100."
+  }
+}
